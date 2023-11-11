f61f650d1ddf3359996582fb4288027b42452f06
Q <1005145q@gmail.com>
2023-11-11 12:58:55 -0600

    fixed database connection

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'capstone_database',
-        'USER': 'root',
-        'PASSWORD': '41#yFbH!',
+        'USER': 'admindjango',
+        'PASSWORD': 'employee@123!',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
