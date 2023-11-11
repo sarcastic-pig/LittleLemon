9ba37e3d4edc73d070ecbcb498be1cee98c509d6
Q <1005145q@gmail.com>
2023-11-11 12:42:52 -0600

    connect to database and create models

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'capstone_database',
+        'USER': 'root',
+        'PASSWORD': '41#yFbH!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -120,7 +127,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    "/restaurant/static/",
+    
 ]
 
 # Default primary key field type
--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=4, decimal_places=2, db_index=True)
+    inventory = models.SmallIntegerField()
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    no_of_guests = models.SmallIntegerField()
+    BookingDate = models.DateTimeField()
